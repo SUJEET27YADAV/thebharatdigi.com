299351e689efba6bdd6a5c3d4ace4fc129bcac35
Sujeet Yadav <sujeet@HP-15-N209TX>
2025-09-29 22:16:03 +0530

    policies added

---
+++ src/app/shipingpolicy/page.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default function Page() {
+  return (
+    <main className="w-full flex flex-col items-center gap-4 p-10">
+      <h1 className="text-4xl font-bold">Shipping Policy</h1>
+      <p>
+        The orders for the user are shipped through registered domestic courier
+        companies and/or speed post only. Orders are shipped within 2 days from
+        the date of the order and/or payment or as per the delivery date agreed
+        at the time of order confirmation and delivering of the shipment,
+        subject to courier company / post office norms. Platform Owner shall not
+        be liable for any delay in delivery by the courier company / postal
+        authority. Delivery of all orders will be made to the address provided
+        by the buyer at the time of purchase. Delivery of our services will be
+        confirmed on your email ID as specified at the time of registration. If
+        there are any shipping cost(s) levied by the seller or the Platform
+        Owner (as the case be), the same is not refundable.
+      </p>
+    </main>
+  );
+}
