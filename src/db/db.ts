45241609147c376a1b0634f3f9d79e0fcdd30103
Sujeet Yadav <sujeet27yadav@gmail.com>
2026-05-16 10:35:39 +0530

    Shop added

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,11 +1,14 @@
-import postgres from 'postgres';
-import { drizzle } from 'drizzle-orm/postgres-js';
+import postgres from "postgres";
+import { drizzle } from "drizzle-orm/postgres-js";
 
 /**
  * Initialize a Postgres connection for server-side database access.
  */
-const sql = postgres(process.env.DATABASE_URL ?? '', {
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: true } : false
+const sql = postgres(process.env.POSTGRES_URL ?? "", {
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? { rejectUnauthorized: true }
+      : false,
 });
 
 export const db = drizzle(sql);
