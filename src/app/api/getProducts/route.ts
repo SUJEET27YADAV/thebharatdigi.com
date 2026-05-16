223aa7b072efac4b9d3164f118fec7363d12c495
Sujeet Yadav <sujeet27yadav@gmail.com>
2026-05-16 18:41:13 +0530

    migrating to supabase

--- src/app/api/getProducts/route.ts
+++ src/app/api/getProducts/route.ts
@@ -1,27 +1,38 @@
-import { db } from '@/db/db';
-import { products } from '@/db/schema';
-import { NextResponse } from 'next/server';
+import { createClient } from "@/utils/supabase/server";
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
+// import { db } from "@/db/db";
+// import { products } from "@/db/schema";
 
 export async function GET() {
-  
-    try {
-        const proArr = await db.select().from(products);
-        if (!proArr || proArr.length === 0) {
-            return NextResponse.json(
-                { success: false, msg: "No products found",data:[] },
-                { status: 404 }
-            );
-        }
-        return NextResponse.json(
-            { success:true, msg:"Products fetched successfully",data:proArr },
-            { status: 200 }
-        );
+  const cookieStore = await cookies();
+  const supabase = createClient(cookieStore);
+  try {
+    const { data, error } = await supabase.from("products").select("*");
+    // const proArr = await db.select().from(products);
+    // if (!proArr || proArr.length === 0) {
+    if (error) {
+      console.error(error);
+      return NextResponse.json(
+        { success: false, msg: "Error fetching products", data: [] },
+        { status: 404 },
+      );
     }
-    catch (error) {
-        console.error(error);
-        return NextResponse.json(
-            { success: false, msg: "Error fetching products" },
-            { status: 500 }
-        );
+    if (!data || data.length === 0) {
+      return NextResponse.json(
+        { success: false, msg: "No products found", data: [] },
+        { status: 404 },
+      );
     }
+    return NextResponse.json(
+      { success: true, msg: "Products fetched successfully", data: data },
+      { status: 200 },
+    );
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { success: false, msg: "Error fetching products" },
+      { status: 500 },
+    );
+  }
 }
