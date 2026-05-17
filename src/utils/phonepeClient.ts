6767f03b42a9329cd020bc78c76f02b3afc1c0f5
SUJEET27YADAV <40136925+SUJEET27YADAV@users.noreply.github.com>
2026-05-17 20:08:06 +0530

    Update phonepeClient.ts

--- src/utils/phonepeClient.ts
+++ src/utils/phonepeClient.ts
@@ -2,8 +2,8 @@ import { StandardCheckoutClient, Env } from "@phonepe-pg/pg-sdk-node";
 
 const clientId = process.env.PHONEPE_CLIENT_ID || "";
 const clientSecret = process.env.PHONEPE_CLIENT_SECRET || "";
-const clientVersion = Number(process.env.PHONEPE_CLIENT_VERSION) || 1;
-const env = Env.SANDBOX; //change to Env.PRODUCTION when you go live
+const clientVersion = process.env.PHONEPE_CLIENT_VERSION || "1";
+const env = Env.PRODUCTION; //change to Env.PRODUCTION when you go live
 
 if (!clientId || !clientSecret) {
   throw new Error("PhonePe Credentials missing in .env");
