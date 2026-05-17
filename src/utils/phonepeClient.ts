7b8b48e3f81e52a0957344c9742d5d64a6cab090
SUJEET27YADAV <40136925+SUJEET27YADAV@users.noreply.github.com>
2026-05-17 19:55:37 +0530

    Update phonepeClient.ts

--- src/utils/phonepeClient.ts
+++ src/utils/phonepeClient.ts
@@ -2,7 +2,7 @@ import { StandardCheckoutClient, Env } from "@phonepe-pg/pg-sdk-node";
 
 const clientId = process.env.PHONEPE_CLIENT_ID || "";
 const clientSecret = process.env.PHONEPE_CLIENT_SECRET || "";
-const clientVersion = process.env.PHONEPE_CLIENT_VERSION || "1";
+const clientVersion = Number(process.env.PHONEPE_CLIENT_VERSION) || 1;
 const env = Env.SANDBOX; //change to Env.PRODUCTION when you go live
 
 if (!clientId || !clientSecret) {
